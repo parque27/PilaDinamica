3c7dd7a4c5a4c62cec2571f5096b6e7b7d06d0ab
parque <pauarqueros27@gmail.com>
2022-04-20 11:24:57 +0200

    Afegida inferfície de PilaFitxes.h

--- PilaDinamica/PilaFitxes.h
+++ PilaDinamica/PilaFitxes.h
@@ -1,5 +1,28 @@
-#pragma once
+#ifndef PILAFITXES_H
+#define PILAFITXES_H
+#include "Fitxa.h"
 class PilaFitxes
 {
-};
+public:
+	PilaFitxes();
+	PilaFitxes(const PilaFitxes& o);
+	~PilaFitxes();
+	PilaFitxes& operator=(const PilaFitxes& o);
 
+	bool es_buida() const;
+	Fitxa cim() const;
+
+	void empila(Fitxa f);
+	void desempila();
+private:
+	struct Node
+	{
+		Fitxa f;
+		Node* seg;
+	};
+	Node* a_cim;
+
+	void copia(const PilaFitxes& o);
+	void allibera();
+};
+#endif // !PILAFITXES_H
