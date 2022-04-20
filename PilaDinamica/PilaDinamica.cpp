74d09e6bec1f8f991e72b5454c67ef11a96f2293
PAU ARQUEROS <pauarqueros27@gmail.com>
2022-04-20 17:27:16 +0200

    Afegit programa a main

--- PilaDinamica/PilaDinamica.cpp
+++ PilaDinamica/PilaDinamica.cpp
@@ -1,8 +1,49 @@
 // ENTRADA:
 // SORTIDA: 
 #include <iostream>
+#include "PilaFitxes.h"
 using namespace std;
+int introduir_opcio()
+{
+	cout << "Introdueix opcio: ";
+	int opcio; cin >> opcio;
+	while (opcio < 1 or opcio > 6)
+	{
+		cout << "Introdueix opcio: ";
+		cin >> opcio;
+	}
+	return opcio;
+}
+void empila(PilaFitxes& p)
+{
+	Fitxa f; f.llegeix();
+	p.empila(f);
+}
+void mostrar_menu()
+{
+	cout << "1) Empilar fitxa" << endl;
+	cout << "2) Desempilar fitxa" << endl;
+	cout << "3) Mostrar fitxa del cim" << endl;
+	cout << "4) Mostrar menu" << endl;
+	cout << "5) Sortir" << endl;
+}
+
 int main()
 {
-	return 0;
+	PilaFitxes p;
+	mostrar_menu();
+	int opcio = introduir_opcio();
+	while (opcio != 5)
+	{
+		if (opcio == 1) empila(p);
+		else if (opcio == 2) p.desempila();
+		else if (opcio == 3)
+		{
+			p.cim().mostra();
+			cout << endl; // ESTETICA
+		}
+		else if (opcio == 4) mostrar_menu();
+
+		opcio = introduir_opcio();
+	}
 }
