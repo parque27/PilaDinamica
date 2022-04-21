a2ee920db80466c1c39e92593449c5c9c597a54d
PAU ARQUEROS <pauarqueros27@gmail.com>
2022-04-21 14:59:52 +0200

    Actualitzats pre i post de main

--- PilaDinamica/PilaDinamica.cpp
+++ PilaDinamica/PilaDinamica.cpp
@@ -1,5 +1,6 @@
 // ENTRADA:
 // SORTIDA: 
+
 #include <iostream>
 #include "PilaFitxes.h"
 using namespace std;
@@ -7,6 +8,8 @@ using namespace std;
 // ACCIONS I FUNCIONS
 int introduir_opcio()
 {
+	// Pre: 1 <= opcio <= 6
+	// Post: retorna l'opcio seleccionada
 	cout << "Introdueix opcio: ";
 	int opcio; cin >> opcio;
 	while (opcio < 1 or opcio > 6)
@@ -18,26 +21,36 @@ int introduir_opcio()
 }
 void empila(PilaFitxes& p)
 {
+	// Pre: --;
+	// Post: llegeix una fitxa i empila la fitxa llegida a p
 	Fitxa f; f.llegeix();
 	p.empila(f);
 }
 void desempila(PilaFitxes& p)
 {
+	// Pre: --
+	// Post: si la pila es buida mostra un missatge, altrament desempila una fitxa
 	if (p.es_buida()) cout << "NO HI HA CAP FITXA" << endl;
 	else p.desempila();
 }
 void mostrar_cim(PilaFitxes& p)
 {
+	// Pre: --
+	// Post: Si la pila es buida mostra un missatge, altrament mostra la pila del cim
 	if (p.es_buida()) cout << "NO HI HA CAP FITXA" << endl;
 	else p.cim().mostra();
 }
 void mostrar_pila(PilaFitxes& p)
 {
+	// Pre: --
+	// Post: si la pila es buida mostra un missatge, altrament mostra les fitxes empilades
 	if (p.es_buida()) cout << "NO HI HA CAP FITXA" << endl;
 	else p.mostra();
 }
 void mostrar_menu()
 {
+	// Pre: --
+	// Post: mostra el menu per pantalla
 	cout << "1) Empilar fitxa" << endl;
 	cout << "2) Desempilar fitxa" << endl;
 	cout << "3) Mostrar fitxa del cim" << endl;
