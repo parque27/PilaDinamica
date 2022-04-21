18d01cdd6d086ac170a3e765f01ea6ab1456822a
PAU ARQUEROS <pauarqueros27@gmail.com>
2022-04-21 14:54:00 +0200

    Actualitzats Pre i Post

--- PilaDinamica/Fitxa.h
+++ PilaDinamica/Fitxa.h
@@ -4,30 +4,32 @@ class Fitxa
 {
 	// DESCRIPCCIO: Fitxa que te un element (aigua, foc, terra, vent i eter) i un valor.
 	// INVARIABLE: Fitxa pren valors de 1 a 20. Caracters que pot ser una fitxa: a,f,t,v,e
-public:
-	// CONSTRUCTORS
-	Fitxa();
-	// Pre: --; Post: la fitxa te tipus buit i valor 0
-	Fitxa(char tipus, int valor);
-	// Pre: --; Post: la fitxa te els parametres passats per constructor
+	public:
+		// CONSTRUCTORS
+		Fitxa();
+		// Pre: --; Post: la fitxa te tipus buit i valor 0
+		Fitxa(char tipus, int valor);
+		// Pre: --; Post: la fitxa te els parametres passats per constructor
 	
-	// OPERADORS
-	bool operator < (const Fitxa& f);
-	// Criteri: this es menor que f si el tipus de this es menor que f, altrament si la numeracio de this es menor que la de f.
+		// OPERADORS
+		bool operator < (const Fitxa& f);
+		// Pre: --; Post: retorna true si aquesta fitxa es menor que f, fals altrament
+			
+		// METODES CONSULTORS
+		void mostra() const;
+		// Pre: --: Post: mostra la fitxa per pantalla
+		bool es_nula() const;
+		// Pre: --; Post: retorna true si la fitxa es nula, fals altrament
+		char tipus_element() const;
+		// Pre: --; Post: retorna el tipus d'element de la fitxa ('a','t','f','v','e')
 	
-	// METODES CONSULTORS
-	void mostra() const;
-	// Pre: --: Post: mostra la fitxa per pantalla
-	bool es_nula() const;
-	// Pre: --; Post: retorna true si la fitxa es nula, fals altrament
-	
-	// METODES MODIFICADORS
-	void llegeix();
-	// Pre: --; Post: llegeix els atributs de la fitxa per teclat
+		// METODES MODIFICADORS
+		void llegeix();
+		// Pre: --; Post: llegeix els atributs de la fitxa per teclat
 
-private:
-	// ATRIBUTS
-	char a_tipus;
-	int a_valor;
+	private:
+		// ATRIBUTS
+		char a_tipus;
+		int a_valor;
 };
 #endif // !FITXA_H
--- PilaDinamica/PilaFitxes.h
+++ PilaDinamica/PilaFitxes.h
@@ -3,25 +3,37 @@
 #include "Fitxa.h"
 class PilaFitxes
 {
+	// DESCRIPCCIO:
+	// INVARIABLE:
+
 	public:
 		// CONSTRUCTORS
 		PilaFitxes();
+		// Pre: --; Post: pila buida
 		PilaFitxes(const PilaFitxes& o);
+		// Pre: --; Post: aquesta pila es una copia de o
 
 		// DESTRUCTOR
 		~PilaFitxes();
+		// Pre: --; Post: memoria alliberada
 
 		// OPERADORS
 		PilaFitxes& operator=(const PilaFitxes& o);
+		// Pre: --; Post: aquesta cua es una copia de o
 
 		// METODES CONSULTORS
 		bool es_buida() const;
+		// Pre: --; Post: retorna true si la pila es buida, fals altrament
 		Fitxa cim() const;
+		// Pre: pila no buida; Post: retorna la fitxa que esta al cim de la pila
 		void mostra() const;
-
+		// Pre: --; Post: mostra la pila de fitxes separades per tres espais
+		
 		// METODES MODIFICADORS
 		void empila(Fitxa f);
+		// Pre: --; Post: ha empilat la fitxa f al cim de la pila
 		void desempila();
+		// Pre: pila no buida; Post: ha desempilat la fitxa del cim de la pila
 
 	private:
 		// ESTRUCTURA DE DADES
@@ -36,6 +48,8 @@ class PilaFitxes
 
 		// METODES D'INSTANCIA
 		void copia(const PilaFitxes& o);
+		// Pre: pila buida; Post: aquesta pila es copia de o
 		void allibera();
+		// Pre: --; Post: pila buida
 };
 #endif // !PILAFITXES_H
